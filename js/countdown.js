532265aac8ca65d66aaab68d1982cc26411cf18d
Dinh Tan Thanh <dinhtanthanh01@gmail.com>
2023-06-01 16:10:35 +0700

    final

---
+++ js/countdown.js
@@ -0,0 +1,34 @@
+var countDownDate = new Date("August 30, 2020 18:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("tm-sale").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("tm-sale").innerHTML = "EXPIRED";
+  }
+}, 1000);
+/*********taikhoan******************/
+document.querySelector('.img-btn').addEventListener('click', function()
+  {
+    document.querySelector('.cont').classList.toggle('s-signup')
+  }
+);
+
